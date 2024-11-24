94661a5adc5c7e594a21c28a245266c8580b9b2c
Mike Roditis <mikeroditis2000@gmail.com>
2024-11-24 16:37:27 +0200

    Make Code a bit Cleaner

--- main.py
+++ main.py
@@ -3,21 +3,27 @@ from State_Controller import state, show_state
 from Algorithms import BFS, Best_First, A_star
 
 colors = get_color_groups()
-start = f'LBUUULBFUULRUFLBRRDDUFDUDURDBLBLRFRRBDLBRFFFDFRBDBDFLL'
+start = state(colors)
 print("Starting state is this: ")
 show_state(start)
 
-print()
-print('#####')
-print()
+print('\n#####\n')
+
 print("Applying Best First: ")
 sol = Best_First(start)
 print(f"Solved in {len(sol)} moves: ")
 print(sol)
+
 print('---')
+
 print("Applying A*: ")
 sol = A_star(start)
 print(f"Solved in {len(sol)} moves: ")
 print(sol)
+
+# print('---')
+#
 # print("Applying BFS: ")
-# print(BFS(start))
+# sol = BFS(start)
+# print(f"Solved in {len(sol)} moves: ")
+# print(sol)
